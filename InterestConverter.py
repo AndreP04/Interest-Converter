963b325de0aaebaf7c0a7244bdee4af38a8157f3
AndreP04 <andrepret4@icloud.com>
2024-02-20 12:28:25 +0200

    Initial Code Added

---
+++ InterestConverter.py
@@ -0,0 +1,9 @@
+#Title of program
+print("\nInterest Converter")
+
+#Display menu of options
+print("\nConvert from:")
+print("\n1. Simple Interest Rate")
+print("2. Effective Annual Interest Rate")
+print("3. Effective Periodic Interest Rate")
+print("4. Nominal Interest Rate")
